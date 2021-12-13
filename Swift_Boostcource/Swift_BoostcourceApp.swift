6ccbe629877ffe528a1b02b0a3905a67025fae9b
jaesjeon <jaesjeon@student.42seoul.kr>
2021-12-13 14:32:49 +0900

    Initial Commit

---
+++ Swift_Boostcource/Swift_BoostcourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_BoostcourceApp.swift
+//  Swift_Boostcource
+//
+//  Created by jaesjeon on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct Swift_BoostcourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
